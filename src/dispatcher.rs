e83c22aa6c149ace77164f6017b31d8853337d57
Thomas Hunter II <me@thomashunter.name>
2024-07-27 09:35:42 -0700

    RAW file woes

--- src/dispatcher.rs
+++ src/dispatcher.rs
@@ -1,7 +1,7 @@
 use clap::ArgMatches;
 
 use crate::config;
-use std::{default, path::Path};
+use std::path::Path;
 
 pub fn dispatch(cmd: ArgMatches) {
     match cmd.subcommand() {
@@ -12,19 +12,20 @@ pub fn dispatch(cmd: ArgMatches) {
         Some(("new", new_matches)) => {
             let path: &String = new_matches.get_one("path").expect("path is required");
             let path = Path::new(path);
-            let default_alias = path
-                .components()
-                .last()
-                .expect("fuck")
-                .as_os_str()
-                .to_str()
-                .expect("shit");
             let alias: &String = match new_matches.get_one("alias") {
-                None => &default_alias.into(),
-                Some(a) => a,
+                None => &path
+                    .components()
+                    .last()
+                    .expect("unable to get new library alias")
+                    .as_os_str()
+                    .to_str()
+                    .expect("unable to parse new library alias")
+                    .into(),
+                Some(given_alias) => given_alias,
             };
             dbg!(path);
             dbg!(alias);
+            // add to config.ini file
         }
 
         _ => unreachable!(),
--- src/main.rs
+++ src/main.rs
@@ -7,3 +7,18 @@ fn main() {
     let cmd = commands::get_commands();
     dispatcher::dispatch(cmd);
 }
+
+#[cfg(test)]
+mod tests {
+  #[test]
+  fn raw_test() {
+    // so this library doesn't support RAW files from my 5 year old camera.
+    // probably going to need to use this C++ library instead.
+    // gonna have to tumble down the rust rabbit hole pretty deep.
+    // https://github.com/felixc/rexiv2/blob/main/SETUP.md
+    let path = "/home/tlhunter/Photographs/San Francisco/2024-02-24 South SF Flickr Walk a7rIV/TLH01595.ARW";
+    let image = rawloader::decode_file(path).unwrap();
+    dbg!(image.width);
+    assert_eq!(image.width, 123);
+  }
+}
